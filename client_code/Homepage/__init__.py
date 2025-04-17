35f7e2c146aa82ee2dafb6ed04cfe46c092107b4
nayakbibhesh1@gmail.com <nayakbibhesh1@gmail.com>
2025-04-17 17:25:03 +0000

    Edited Homepage, settings, ServerModule1 and Form3

--- client_code/Homepage/__init__.py
+++ client_code/Homepage/__init__.py
@@ -1,5 +1,11 @@
 from ._anvil_designer import HomepageTemplate
 from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
 import anvil.server
 
 
@@ -7,7 +13,20 @@ class Homepage(HomepageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    if anvil.users.get_user():
+      self.button_5.visible = False
 
     # Any code you write here will run before the form opens.
+  def button_5_click(self,**event_args):
+    user = anvil.users.login_with_form(show_signup_option=True ,allow_cancel=True)
+    if user:
+      open_form('Homepage')
+    
   def button_3_click(self,**event_args):
     open_form('Form3')
+
+  def button_7_click(self, **event_args):
+    anvil.users.logout()
+    self.button_5.visible = True
+    self.button_7.visible = False
+    
